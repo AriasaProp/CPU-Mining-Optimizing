a7efc73ea162a24bc9ba2c5379a88e816cc16c51
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2023-01-21 08:26:31 +0800

    first

---
+++ desktop/src/main/java/com/ariasaproject/cpumininglearn/app/App.java
@@ -0,0 +1,18 @@
+package com.ariasaproject.cpumininglearn.app;
+
+import com.ariasaproject.cpumininglearn.list.LinkedList;
+
+import static com.ariasaproject.cpumininglearn.utilities.StringUtils.join;
+import static com.ariasaproject.cpumininglearn.utilities.StringUtils.split;
+import static com.ariasaproject.cpumininglearn.app.MessageUtils.getMessage;
+
+import org.apache.commons.text.WordUtils;
+
+public class App {
+    public static void main(String[] args) {
+        LinkedList tokens;
+        tokens = split(getMessage());
+        String result = join(tokens);
+        System.out.println(WordUtils.capitalize(result));
+    }
+}
