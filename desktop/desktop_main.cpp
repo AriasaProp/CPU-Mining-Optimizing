7bc670975537c07ec621a653242c59d7d8f0369c
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2023-01-26 07:26:20 +0800

    fix-binary

--- desktop/desktop_main.cpp
+++ desktop/desktop_main.cpp
@@ -12,16 +12,12 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     wc.hInstance     = hInstance;
     wc.lpszClassName = CLASS_NAME;
     RegisterClass(&wc);
-    HWND hwnd = CreateWindowEx(
-        0, CLASS_NAME, L"Learn to Program Windows", WS_OVERLAPPEDWINDOW, 
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-        NULL, NULL, hInstance, NULL);
-    if (hwnd == NULL) {
+    HWND hwnd = CreateWindowEx(0, CLASS_NAME, L"Learn to Program Windows", WS_OVERLAPPEDWINDOW,  CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,0, 0, hInstance, 0);
+    if (!hwnd)
         return 0;
-    }
     ShowWindow(hwnd, nCmdShow);
     MSG msg = { };
-    while (GetMessage(&msg, NULL, 0, 0) > 0) {
+    while (GetMessage(&msg, 0, 0, 0) > 0) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
