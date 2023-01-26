5ec599c46f7081fd2e5b0e607c3a5da159cecaee
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2023-01-26 20:06:03 +0800

    fix-jni10

--- android/src/main/cpp/main.cpp
+++ android/src/main/cpp/main.cpp
@@ -20,8 +20,8 @@ JNI_Call(void, write) (JNIEnv* env, jclass, jint lv, jstring l) {
 	//const jsize *cstr_length = env->GetStringLength(l);
   char buf[10];
   std::time_t t = std::time(0);
-  if(strftime(buf, 10, "hh:mm:ss| ", std::localtime(&t))){
-  	logs[0] = std::string() + buf + cstr;
+  if(strftime(buf, sizeof(buf), "%H:%M:%S| ", std::localtime(&t))){
+  	logs[0] = std::string(buf) + std::string(cstr);
   } else {
   	logs[0] = "problem in write native i guest";
   }
