6b54b04193e38466cded03921304dfb709e8ebaa
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2023-01-28 20:40:43 +0800

    l29

--- core/console_log.cpp
+++ core/console_log.cpp
@@ -16,37 +16,36 @@ void console_log::initialize() {
 	endHtmlMsg = htmlMsg+console_log::MAX_MSG_SIZE;
 	write(0,"Wellcome to CPU Mining Opt", 27);
 }
-
 const char *console_log::write(unsigned int lv, const char *msg, unsigned long length) {
-	char tmsg[10];
-  std::time_t t = std::time(0);
-  if(!strftime(tmsg, 10, "%T", std::localtime(&t))){
-  	memcpy(tmsg, "Error!!   ", 10);
-  }
-	char tx_clr[6];
+  memmove(htmlMsg+length+41, htmlMsg, console_log::MAX_MSG_SIZE-length-41);
+  char *modif = htmlMsg;
+	memcpy(modif, "<font color='#", 14);
+	modif += 14;
 	switch(lv) {
 		default:
 		case 0:
-			memcpy(tx_clr,"808080",6);//debug
+			memcpy(modif,"808080",6);//debug
 			break;
 		case 1:
-			memcpy(tx_clr,"0000ff",6);//info
+			memcpy(modif,"0000ff",6);//info
 			break;
 		case 2:
-			memcpy(tx_clr,"00ff00",6);//success
+			memcpy(modif,"00ff00",6);//success
 			break;
 		case 3:
-			memcpy(tx_clr,"ffff00",6);//warning
+			memcpy(modif,"ffff00",6);//warning
 			break;
 		case 4:
-			memcpy(tx_clr,"ff0000",6);//Error
+			memcpy(modif,"ff0000",6);//Error
 			break;
   }
-  size_t l_move = 50+length;
-  char buff[l_move];
-  sprintf(buff, "<font color='#%s'>%s| %s</font><br>", tx_clr, tmsg, msg);
-  memmove(htmlMsg+l_move, htmlMsg, console_log::MAX_MSG_SIZE-l_move);
-  memcpy(htmlMsg, buff, l_move);
+  *(modif+=6) = '>';
+  std::time_t t = std::time(0);
+  if(!strftime((++modif), 9, "%T", std::localtime(&t))){
+  	memcpy(modif, "Error!!   ", 9);
+  }
+  memcpy((modif+=9), msg, length);
+  memcpy((modif+=length), "</font><br>", 11);
   
 	return htmlMsg;
 }
