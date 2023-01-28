ac8e8b2f7fc1f83be3e9a8a9fba0f51b050432c3
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2023-01-28 22:31:00 +0800

    l33

--- core/console_log.cpp
+++ core/console_log.cpp
@@ -15,10 +15,12 @@ void console_log::initialize() {
 	endHtmlMsg = htmlMsg+console_log::MAX_MSG_SIZE;
 	*endHtmlMsg = '\0';
 }
+const char *frontKey = "<font color='#";
+const char *endKey = "</font><br>";
 const char *console_log::write(unsigned int lv, const char *msg, unsigned long length) {
   memmove(htmlMsg+length+43, htmlMsg, console_log::MAX_MSG_SIZE-length-45);
   char *modif = htmlMsg;
-	memcpy(modif, "<font color='#", 14);
+	memcpy(modif, frontKey, 14);
 	modif += 14;
 	switch(lv) {
 		default:
@@ -44,7 +46,20 @@ const char *console_log::write(unsigned int lv, const char *msg, unsigned long l
   	memcpy(modif, "Error!!!!  ", 11);
   }
   memcpy((modif+=10), msg, length);
-  memcpy((modif+=length), "</font><br>", 11);
+  memcpy((modif+=length), endKey, 11);
+  //check if some cutted html line
+  char *tF = endHtmlMsg-14;
+  while (--tF > modif) {
+  	if (memcmp(tF, frontKey, 14) == 0) {
+  		char *bF = strstr(tF, endKey);
+  		if (bF && ((bF+endKey+1)<endHtmlMsg)) {
+  				tF = bF+endKey
+  			}
+  		}
+			memset(tF, ' ', endHtmlMsg-tF)
+  		break;
+  	}
+  }
 	return htmlMsg;
 }
 
