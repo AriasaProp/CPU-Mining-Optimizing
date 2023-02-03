8cd3a1fd9ad3602efa1bb9b5a5ec4ed669f5e90b
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2023-02-03 15:52:38 +0800

    Thread Management

--- core/core.cpp
+++ core/core.cpp
@@ -1,72 +1,58 @@
 #include "core.h"
 
 #include <iostream>
-#include <unistd.h>
-#include <pthread.h>
+#include <thread>
+#include <condition_variable>
+#include <chrono>
+#include <mutex>
 
-pthread_mutex_t mutex;
-pthread_cond_t cond;
-pthread_t thread;
+std::mutex mutex;
+std::condition_variable cv;
+std::thread thread;
 
-static void *miningThread(void*);
 struct data_transfer {
 	bool create;
 	bool destroy;
 } *loc_data;
 
+void miningThread();
+
 void core::startMining(void*) {
+	if(loc_data) return;
 	loc_data = new data_transfer;
-	pthread_mutex_init(&mutex, NULL);
-  pthread_cond_init(&cond, NULL);
-  pthread_attr_t attr; 
-  pthread_attr_init(&attr);
-  pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
-  loc_data->create = true;
-  pthread_create(&thread, &attr, miningThread, loc_data);
-  pthread_attr_destroy(&attr);
-  pthread_mutex_lock(&mutex);
-  while(loc_data->create)
-  	pthread_cond_wait(&cond,&mutex);
-  pthread_mutex_unlock(&mutex);
-  
+	loc_data->create = true;
+	thread = std::thread(miningThread);
+	std::unique_lock<std::mutex> lck(mutex);
+	cv.wait(lck, []{return !loc_data->create;});
 }
 
 void core::stopMining(void*) {
-  pthread_mutex_lock(&mutex);
-  loc_data->destroy = true;
-  while(loc_data->destroy)
-  	pthread_cond_wait(&cond,&mutex);
-  pthread_mutex_unlock(&mutex);
-  pthread_cond_destroy(&cond);
-  pthread_mutex_destroy(&mutex);
-  delete loc_data;
+	if(!loc_data) return;
+	loc_data->destroy = true;
+	thread.join();
+	delete loc_data;
 }
 
-static void *miningThread(void *dat) {
-	data_transfer *dt = (data_transfer*)dat;
-	
+void miningThread() {
 	//this for preparation like socket validation auth etc.
-	sleep(3);
-  pthread_mutex_lock(&mutex);
-  dt->create = false;
-	pthread_cond_broadcast(&cond);
-  pthread_mutex_unlock(&mutex);
-	
-	
+	std::this_thread::sleep_for(std::chrono::seconds(3));
+	std::unique_lock<std::mutex> lck(mutex);
+	loc_data->create = false;
+	cv.notify_one();
+	lck.unlock();
+
 	for(;;) {
-		//do nothing right now
-		sleep(1); 
-	  pthread_mutex_lock(&mutex);
-  	if (dt->destroy)
+		std::unique_lock<std::mutex> lck(mutex);
+		if (loc_data->destroy)
 			break;
-	  pthread_mutex_unlock(&mutex);
+		lck.unlock();
+		//do nothing right now
+		std::this_thread::sleep_for(std::chrono::seconds(1)); 
 	}
-	
+
 	//this for cleaning like socket close etc.
-	sleep(3);
-  pthread_mutex_lock(&mutex);
-  dt->destroy = false;
-	pthread_cond_broadcast(&cond);
-  pthread_mutex_unlock(&mutex);
-  return NULL;
+	std::this_thread::sleep_for(std::chrono::seconds(3));
+	std::unique_lock<std::mutex> lck(mutex);
+	loc_data->destroy = false;
+	lck.unlock();
 }
