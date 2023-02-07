0775faccdb6e9dc39cc9acded399afe31041938c
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2023-02-07 08:12:14 +0800

    Adding Console 15

--- android/src/main/cpp/connection/androidminingconnection.h
+++ android/src/main/cpp/connection/androidminingconnection.h
@@ -5,21 +5,20 @@
 #include <pthread.h>
 
 struct AndroidSocket: public MiningSocket {
-private:
+public:
 	bool ready;
 	bool hasConnection;
 	pthread_mutex_t mutex;
 	pthread_cond_t cond;
 	pthread_t thread;
 	sockaddr_in server_addr;
-public:
 	AndroidSocket();
 	~AndroidSocket();
 	bool openConnection(const char*, unsigned int&) override;
 	bool write(const char*) override;
 	char *read() override;
 	bool closeConnection() override;
-	friend static void *socketLoop(void*); //access private value
+	friend static void *socketLoop(void*);
 };
 
 #endif //_AndroidMiningConnection_Included
