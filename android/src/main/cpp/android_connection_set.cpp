fd04df0180ded3fecb1e9992ccdb46ecc519ccb0
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2023-02-09 21:44:08 +0800

    set connection 21

--- android/src/main/cpp/android_connection_set.cpp
+++ android/src/main/cpp/android_connection_set.cpp
@@ -82,7 +82,7 @@ char recvBuffer[2048];
 const char *_recvConnection() {
 	if (!_hasConnection) return "No connection already!";
 	for(unsigned int i = 0; i < 3; i++) {
-		unsigned int length = recv(socketFd, recvBuffer, 2048, MSG_WAITALL);
+		int length = recv(socketFd, recvBuffer, 2048, MSG_WAITALL);
 		if (length > 0) {
 			recvBuffer[length] = '\0';
 			return recvBuffer;
