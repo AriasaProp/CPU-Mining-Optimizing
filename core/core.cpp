0f400a554615edb81fe48b08a1a2d778f701822e
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2023-02-15 09:32:30 +0800

    test_result4

--- core/core.cpp
+++ core/core.cpp
@@ -106,10 +106,10 @@ void miningThread() {
 				break;
 			}
 			console::write(0, "No message");
-			std::this_thread::sleep_for(std::chrono::milliseconds(100)); 
+			std::this_thread::sleep_for(std::chrono::seconds(1)); 
 		}
 		if (trying >= 3) {
-			throw "No received message after subscribe";
+			throw "No received message after authorize";
 		}
 		console::write(0, "Authorize succes");
 		function_set::afterStart();
